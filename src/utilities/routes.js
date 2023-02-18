650de5006736008af36735d5ba523b791166dc19
Hasan Imam <hasanjimam@gmail.com>
2023-02-18 21:57:01 +0900

    Header added

---
+++ src/utilities/routes.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Header from "../components/Header";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Header></Header>
+        
+    }
+])
+
+export default router;
