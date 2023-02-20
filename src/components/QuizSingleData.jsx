b497ed176a77dcd742236a68e24b5897e9591704
hasanisu <hasanjimam@gmail.com>
2023-02-20 17:24:18 +0900

    quiz sectin update

--- src/components/QuizSingleData.jsx
+++ src/components/QuizSingleData.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const QuizSingleData = ({ qdata }) => {
     const { options, question, correctAnswer } = qdata;
-    console.log(options);
+
 
    const [data, setData] = useState();
 
-   let massgae;
-   
-   let value;
-   if(value === {correctAnswer}){
-        massgae = toast("Wow so easy!");
-   }
+    const handleToAnswer=()=>{
+        toast(`Correct Answer: ${correctAnswer}`);
+        
+    }
    
 
 
-//    {correctAnswer ? alert('you are rigth'): <p>slectst</p>}
+//  
 
     return (
         <div>
             <h2>Question: {question}</h2>
             <h1>{data}</h1>
             <div>
+            
 
                 <div className=' bg-orange-600 w-6/12 mx-auto  rounded-md mb-11'>
+                <button onClick={handleToAnswer}><FontAwesomeIcon icon={faCoffee} /></button>
                     {
                     options.map(option =>
                     <div>
